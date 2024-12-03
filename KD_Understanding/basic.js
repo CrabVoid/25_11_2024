c6c3367799ae0899e25c984ee5df6307a72e5296
crab_void <you@example.com>
2024-12-03 08:06:09 +0200

    03_12_2024

---
+++ KD_Understanding/basic.js
@@ -0,0 +1,36 @@
+//let personAge = 17;
+
+/*if (personAge  >= 18) {
+    console.log("pilngadīgs")
+} else {
+    console.log("nepilgadīgs")
+}       */
+
+/*function checkAge(Age) {
+    if (personAge  >= 18) {
+        console.log("pilngadīgs")
+    } else {
+        console.log("nepilgadīgs")
+    }       
+}
+
+//console.log(checkAge(personAge))*/
+
+/*let namesArray = ["Antra", "Jānis", "Artūrs"];
+
+for (let i = 0; i < namesArray.length; i++) {
+    console.log(i);
+    console.log(namesArray[i])
+}
+
+function sayHello(name) {
+    return "labdien," + '$(name)!';
+}
+
+sayHello("elza")
+console*/
+//function printErrorStatus(code, text) {
+    return `${code}` - `${text}` ;
+//}
+
+//console.log(printErrorStatus("Bad Gateway", 502));
